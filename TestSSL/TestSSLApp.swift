3a64da90b5d5999791615fbaa687586b0b389d04
Ritesh Nemade <ritesh.nemade@datafortune.com>
2024-09-13 18:29:18 +0530

    Initial Commit

---
+++ TestSSL/TestSSLApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestSSLApp.swift
+//  TestSSL
+//
+//  Created by Ritesh on 13/09/24.
+//
+
+import SwiftUI
+
+@main
+struct TestSSLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
